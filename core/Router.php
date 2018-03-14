c06717267d3bd9fba7e8c183dbf5b6550ea5e692
Paul <paul@Pauls-Macbook.local>
2018-03-14 12:10:22 -0500

    vid-20

--- core/Router.php
+++ core/Router.php
@@ -2,7 +2,10 @@
 
 class Router
 {
-    protected $routes = [];
+    protected $routes = [
+      'GET' => [],
+      'POST' => []
+    ];
 
     public static function load($file)
     {
@@ -11,17 +14,22 @@ class Router
         return $router;
     }
 
-    public function define($routes)
+    public function get($uri, $controller)
     {
-        $this->routes = $routes;
+      $this->routes['GET'][$uri] = $controller;
     }
 
-    public function direct($uri)
+    public function post($uri, $controller)
     {
-        if (array_key_exists($uri, $this->routes)) {
-            return $this->routes[$uri];
+      $this->routes['POST'][$uri] = $controller;
+    }
+
+    public function direct($uri, $method)
+    {
+        if (array_key_exists($uri, $this->routes[$method])) {
+            return $this->routes[$method][$uri];
         }
 
-        throw new Exception('No routes found');
+        throw new Exception('No routes found' . $uri);
     }
 }
